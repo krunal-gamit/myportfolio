c057c09070f78ee6c202dced31c5bbe56d8bbd44
krooks_geek <krooksgeek@gmail.com>
2021-08-10 22:42:05 +0530

    Banner Update

--- src/ThirdBanner.js
+++ src/ThirdBanner.js
@@ -6,35 +6,16 @@ function ThirdBanner(){
             <div className='bannerTitle'>
                 <div id='t1'>
                     <h1>LATEST</h1>
+                    </div>
                 <div id='t2'>
                     <h1>WORK</h1>
-                </div>
+                
                 </div>
             </div>
         </div>
     );
 }
 
-
-let atScroll = false;
-let parallaxTitle = document.querySelectorAll(".bannerTitle");
-
-const scrollProgress = () => {
-    atScroll = true;
-};
-
-const raf = () => {
-    if (atScroll) {
-        parallaxTitle.forEach((element, index) => {
-            element.style.transform = "translateX(" + window.scrollY / 8 + "%)";
-        });
-        atScroll = false;
-    }
-    requestAnimationFrame(raf);
-};
-
-requestAnimationFrame(raf);
-window.addEventListener("scroll", scrollProgress);
 
 
 
