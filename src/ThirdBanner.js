4ce0aa349c1a307a033dace873533f015d7195e3
krooks_geek <krooksgeek@gmail.com>
2021-08-11 14:04:33 +0530

    Updating third banner

--- src/ThirdBanner.js
+++ src/ThirdBanner.js
@@ -2,16 +2,50 @@ import './ThirdBanner.css'
 
 function ThirdBanner(){
     return(
-        <div className='bannerBack'>
-            <div className='bannerTitle'>
-                <div id='t1'>
-                    <h1>LATEST</h1>
-                    </div>
-                <div id='t2'>
-                    <h1>WORK</h1>
+        <div class="bannerb">
+            <div class="content">
+                <div class="container">
+                    
+                        <h2 class="text text-dark">
+                            <span class="parallax-title">
+                                we live in the details
+                            </span>
+                        </h2>
+                    
+
+                    <h2 class="text">
+                        <span class="parallax-title">
+                            we live in the details
+                        </span>
+                    </h2>
                 </div>
             </div>
         </div>
+        
+        
+    
     );
+    
 }
+
+let atScroll = false;
+        let parallaxTitle = document.querySelectorAll(".parallax-title");
+
+        const scrollProgress = () => {
+            atScroll = true;
+        };
+
+        const raf = () => {
+            if (atScroll) {
+                parallaxTitle.forEach((element, index) => {
+                    element.style.transform = "translateX(" + window.scrollY / 18 + "%)";
+                });
+                atScroll = false;
+            }
+            requestAnimationFrame(raf);
+        };
+
+        requestAnimationFrame(raf);
+        window.addEventListener("scroll", scrollProgress);
+
 export default ThirdBanner;
