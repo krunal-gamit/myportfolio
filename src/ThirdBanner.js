6dd062393a8bf2b266538b2a969e429a2aa6f5ac
krooks_geek <krooksgeek@gmail.com>
2021-08-14 17:33:18 +0530

    working on the banner & shapes

--- src/ThirdBanner.js
+++ src/ThirdBanner.js
@@ -3,19 +3,15 @@ import './ThirdBanner.css'
 function ThirdBanner(){
     return(
         <div className='bannerBack'>
-            <div className='bannerTitle'>
-                <div id='t1'>
-                    <h1>LATEST</h1>
-                    </div>
-                <div id='t2'>
-                    <h1>WORK</h1>
-                
+            <div className='bannerContainer'>
+                <div className='bannerText'>
+                    <span className="latest">LATEST</span>
+                    <span className="work">WORK</span>
                 </div>
             </div>
         </div>
     );
 }
-
 
 
 
