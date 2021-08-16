3f508ad544e58cca6bc6081563811139ac154322
krooks_geek <krooksgeek@gmail.com>
2021-08-16 18:25:11 +0530

    made changes to the 3d section

--- src/Four3D.js
+++ src/Four3D.js
@@ -7,9 +7,9 @@ function Four3D(){
             <iframe src='https://my.spline.design/untitled-2d96300fdf92256f7e4599e28ac185e3/' frameborder='0' width='100%' height='100%'></iframe>
         </div>
             <div className='contentText'>
-                <span id="textone">Have You Tried
-                    <br></br> Spline tool? <br></br>
-                </span>
+                <span id="textone">Guess this tool
+                    <br></br></span>
+                    <span id="midtext"> can you?</span> <br></br>
                 <span id="texttwo">
                     (Drag to rotate, hover to spin)
                 </span>
