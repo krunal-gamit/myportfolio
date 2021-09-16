da9488559c5f79ccacdbf3c1d4d99bb075b28dc4
krooks_geek <krooksgeek@gmail.com>
2021-09-16 23:16:39 +0530

    new changes to about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./About.css"
+import SixthMeet from "./SixthMeet";
 
 function About() {
   return (
     <div className="about">
-      <div class="container">
         <div className="aboutName">
             <span id="name">Krunal Gamit</span>
         </div>
         <div className="aboutText">
             <span id="abt-One">
             I'm currently pursuing B.Tech Computer Science &#38; Engineering degree at Nirma University, Ahmedabad.
-I love designing and developing web applications, I work on it till it becomes cool stuff. Apart from software, I like music. I'm quite an intermediate guitarist.
+            I love designing and developing web applications, 
+            I work on it till it becomes cool stuff. Apart from software, I like music. 
+            I'm quite an intermediate guitarist.
             </span>
             <span id="abt-Two">
             I have a passion for developing better solutions and user experience that are impactful.
@@ -23,16 +25,15 @@ I love designing and developing web applications, I work on it till it becomes c
       
       <div className="toolsSec">
         <span id="toolTitle">Tech &#38; Tools</span>
-        <span id="tools">
+        <div id="tools">
           <span className="toolrow">
                 <ul>
                   <li>Java</li>
                   <li>C</li>
                   <li>Python</li>
-                  <li>Git</li>
-                  <li>Heroku</li>
                   <li>HTML</li>
                   <li>CSS</li>
+                  <li>PHP</li>
                   <li>Reactjs</li>
                   <li>Framer Motion</li>
                 </ul>
@@ -45,12 +46,47 @@ I love designing and developing web applications, I work on it till it becomes c
                   <li>After Effects</li>
                   <li>Premier Pro</li>
                 </ul>
-
+                <ul>
+                  <li>Git</li>
+                  <li>Heroku</li>
+                  <li>Metlify</li>
+                  <li>Docker</li>
+                  <li>Google Cloud</li>
+                </ul>
           </span>
-        </span>
+        </div>
       </div>
 
+
+      <div className="toolsSec">
+        <span id="toolTitle">Current Favorites</span>
+        <div id="tools2">
+          <span className="toolrow2">
+                <ul>
+                  <li>Programming Language</li>
+                  <li>Editor</li>
+                  <li>Version Control</li>
+                  <li>Design Framework</li>
+                  <li>Design Tool</li>
+                  <li>Fonts</li>
+                  <li>UX/UI Inspiration</li>
+                  
+                </ul>
+                <ul>
+                  <li>Java</li>
+                  <li>Vim, VS Code, Sublime</li>
+                  <li>Git, github</li>
+                  <li>Reactjs</li>
+                  <li>XD, Figma</li>
+                  <li>Codec, SF Pro Display, GT Super Display, Raleway</li>
+                  <li>Awwwards</li>
+                </ul>
+          </span>
+        </div>
       </div>
+
+      <SixthMeet/>
+      <div className="space"></div>
     </div>
   );
 }
