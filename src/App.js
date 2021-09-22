80c4c294cef6f3db8954262d338e917196c8dbba
krooks_geek <krooksgeek@gmail.com>
2021-09-22 22:15:51 +0530

    fixed the scroll to top

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ import ProjectOne from "./components/ProjectOne";
 import ProjectTwo from "./components/ProjectTwo";
 import ProjectThree from "./components/ProjectThree";
 import ProjectFour from "./components/ProjectFour";
+import ScrollToTop from "./components/ScrollToTop";
 
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop/>
       <Header/>
       <Switch>
           <Route path="/" exact component={() => <Home />} />
