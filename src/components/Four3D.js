23b345224a7cec927f16c773ba872fb60ea81d42
krooks_geek <krooksgeek@gmail.com>
2022-01-01 23:57:55 +0530

    adding insta page

--- src/components/Four3D.js
+++ src/components/Four3D.js
@@ -2,22 +2,36 @@ import './Four3D.css'
 
 function Four3D(){
     return(
-        /*
-        <div className="content3d">
-            <div className="contain">
-            
-        </div>
-            <div className='contentText'>
-                <span id="textone">
-                    <br></br></span>
-                    <span id="midtext"> </span> <br></br>
-                <span id="texttwo">
-                    
-                </span>
+        
+       <div className='mainBody'>
+           <section>
+               <div className='perspective containerr'>
+               <div className=' grid grid--columns'>
+                   <div>
+            <h2 className='fs-700 uppercase text-compressed'>INSTAGRAM FEED</h2>
+            <p>The feed will smoothly transition into the next.<br/> The images vertically and horizontally overlain objects looks connected.<br/> Inspired by Casey McPerry (@caseymcperry). </p>
+
+            <div className='flex large-gap '>
+                <a target='_blank' href='https://instagram.com/krunalgamit_' className='btn btn--primary uppercase text-compressed'>VISIT PROFILE</a>
+                <a href='/' className='btn btn--accent uppercase'><img src="/img/11.jpg"></img></a>
             </div>
-        </div>
-        */
-       <div></div>
+            </div>
+            </div>
+
+            <div className='grid feature-grid'>
+                <a href='#' aria-label='check'><img src="/img/1.jpg"></img></a>
+                <a href='#'aria-label=''><img src="/img/2.jpg"></img></a>
+                <a href='#'aria-label=''><img src="/img/3.jpg"></img></a>
+                <a href='#'aria-label=''><img src="/img/4.jpg"></img></a>
+                <a href='#'aria-label=''><img src="/img/5.jpg"></img></a>
+                <a href='#'aria-label=''><img src="/img/6.jpg"></img></a>
+                <a href='#'aria-label=''><img src="/img/7.jpg"></img></a>
+                <a href='#'aria-label=''><img src="/img/8.jpg"></img></a>
+            </div>
+
+            </div>
+           </section>
+       </div>
     );
 }
 
